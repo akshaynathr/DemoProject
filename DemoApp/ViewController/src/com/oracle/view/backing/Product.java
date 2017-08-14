89584ce0a313804bf17922290b847fdff1ad6127
Deepthy S Nair <deeptnai@oradev.oraclecorp.com>
2017-08-14 19:23:06 +0530

    Products form

---
+++ DemoApp/ViewController/src/com/oracle/view/backing/Product.java
@@ -0,0 +1,104 @@
+package com.oracle.view.backing;
+
+import javax.faces.component.UISelectItems;
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.input.RichInputText;
+import oracle.adf.view.rich.component.rich.input.RichSelectOneChoice;
+import oracle.adf.view.rich.component.rich.layout.RichPanelFormLayout;
+import oracle.adf.view.rich.component.rich.nav.RichButton;
+import oracle.adf.view.rich.component.rich.output.RichMessages;
+
+public class Product {
+    private RichForm f1;
+    private RichDocument d1;
+    private RichMessages m1;
+    private RichPanelFormLayout pfl1;
+    private RichInputText it1;
+    private RichInputText it2;
+    private RichInputText it3;
+    private RichSelectOneChoice soc1;
+    private UISelectItems si1;
+    private RichButton b1;
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+
+    public void setM1(RichMessages m1) {
+        this.m1 = m1;
+    }
+
+    public RichMessages getM1() {
+        return m1;
+    }
+
+    public void setPfl1(RichPanelFormLayout pfl1) {
+        this.pfl1 = pfl1;
+    }
+
+    public RichPanelFormLayout getPfl1() {
+        return pfl1;
+    }
+
+    public void setIt1(RichInputText it1) {
+        this.it1 = it1;
+    }
+
+    public RichInputText getIt1() {
+        return it1;
+    }
+
+    public void setIt2(RichInputText it2) {
+        this.it2 = it2;
+    }
+
+    public RichInputText getIt2() {
+        return it2;
+    }
+
+    public void setIt3(RichInputText it3) {
+        this.it3 = it3;
+    }
+
+    public RichInputText getIt3() {
+        return it3;
+    }
+
+    public void setSoc1(RichSelectOneChoice soc1) {
+        this.soc1 = soc1;
+    }
+
+    public RichSelectOneChoice getSoc1() {
+        return soc1;
+    }
+
+    public void setSi1(UISelectItems si1) {
+        this.si1 = si1;
+    }
+
+    public UISelectItems getSi1() {
+        return si1;
+    }
+
+    public void setB1(RichButton b1) {
+        this.b1 = b1;
+    }
+
+    public RichButton getB1() {
+        return b1;
+    }
+}
