16527bd08d0099952ea161e3ff0895596a56df55
Doyle Fermi <doyle.fermi@oracle.com>
2017-08-22 18:51:18 +0530

    Completed insertion to Customers and linked tables

--- ViewController/src/view/NewCustBeanClass.java
+++ ViewController/src/view/NewCustBeanClass.java
@@ -2,7 +2,6 @@ package view;
 
 import oracle.adf.view.rich.component.rich.RichDocument;
 import oracle.adf.view.rich.component.rich.RichForm;
-import oracle.adf.view.rich.component.rich.input.RichInputDate;
 import oracle.adf.view.rich.component.rich.input.RichInputText;
 import oracle.adf.view.rich.component.rich.layout.RichPanelFormLayout;
 import oracle.adf.view.rich.component.rich.layout.RichPanelStretchLayout;
@@ -26,42 +25,23 @@ public class NewCustBeanClass {
     private RichButton b1;
     private RichMessages m1;
     private RichPanelFormLayout pfl1;
-    private RichInputText it1;
     private RichInputText it2;
     private RichInputText it3;
-    private RichInputText it4;
     private RichInputText it5;
     private RichInputText it6;
     private RichInputText it7;
     private RichInputText it8;
     private RichInputText it9;
     private RichInputText it10;
-    private RichInputText it11;
-    private RichInputText it12;
-    private RichInputText it13;
-    private RichInputDate id1;
-    private RichInputText it14;
-    private RichInputDate id2;
-    private RichInputText it15;
-    private RichInputText it16;
-    private RichInputText it17;
     private RichInputText it18;
     private RichInputText it19;
     private RichInputText it20;
     private RichInputText it21;
     private RichInputText it22;
     private RichInputText it23;
-    private RichInputText it24;
     private RichInputText it25;
     private RichInputText it26;
     private RichInputText it27;
-    private RichInputText it28;
-    private RichInputDate id3;
-    private RichInputText it29;
-    private RichInputDate id4;
-    private RichInputText it30;
-    private RichInputText it31;
-    private RichInputText it32;
     private RichInputText it33;
     private RichInputText it34;
     private RichInputText it35;
@@ -69,8 +49,6 @@ public class NewCustBeanClass {
     private RichInputText it37;
     private RichInputText it38;
     private RichInputText it39;
-    private RichInputText it40;
-    private RichInputText it41;
 
     public NewCustBeanClass() {
     }
@@ -123,13 +101,6 @@ public class NewCustBeanClass {
         return pfl1;
     }
 
-    public void setIt1(RichInputText it1) {
-        this.it1 = it1;
-    }
-
-    public RichInputText getIt1() {
-        return it1;
-    }
 
     public void setIt2(RichInputText it2) {
         this.it2 = it2;
@@ -147,13 +118,6 @@ public class NewCustBeanClass {
         return it3;
     }
 
-    public void setIt4(RichInputText it4) {
-        this.it4 = it4;
-    }
-
-    public RichInputText getIt4() {
-        return it4;
-    }
 
     public void setIt5(RichInputText it5) {
         this.it5 = it5;
@@ -203,77 +167,6 @@ public class NewCustBeanClass {
         return it10;
     }
 
-    public void setIt11(RichInputText it11) {
-        this.it11 = it11;
-    }
-
-    public RichInputText getIt11() {
-        return it11;
-    }
-
-    public void setIt12(RichInputText it12) {
-        this.it12 = it12;
-    }
-
-    public RichInputText getIt12() {
-        return it12;
-    }
-
-    public void setIt13(RichInputText it13) {
-        this.it13 = it13;
-    }
-
-    public RichInputText getIt13() {
-        return it13;
-    }
-
-    public void setId1(RichInputDate id1) {
-        this.id1 = id1;
-    }
-
-    public RichInputDate getId1() {
-        return id1;
-    }
-
-    public void setIt14(RichInputText it14) {
-        this.it14 = it14;
-    }
-
-    public RichInputText getIt14() {
-        return it14;
-    }
-
-    public void setId2(RichInputDate id2) {
-        this.id2 = id2;
-    }
-
-    public RichInputDate getId2() {
-        return id2;
-    }
-
-    public void setIt15(RichInputText it15) {
-        this.it15 = it15;
-    }
-
-    public RichInputText getIt15() {
-        return it15;
-    }
-
-    public void setIt16(RichInputText it16) {
-        this.it16 = it16;
-    }
-
-    public RichInputText getIt16() {
-        return it16;
-    }
-
-    public void setIt17(RichInputText it17) {
-        this.it17 = it17;
-    }
-
-    public RichInputText getIt17() {
-        return it17;
-    }
 
     public void setIt18(RichInputText it18) {
         this.it18 = it18;
@@ -323,13 +216,6 @@ public class NewCustBeanClass {
         return it23;
     }
 
-    public void setIt24(RichInputText it24) {
-        this.it24 = it24;
-    }
-
-    public RichInputText getIt24() {
-        return it24;
-    }
 
     public void setIt25(RichInputText it25) {
         this.it25 = it25;
@@ -355,61 +241,6 @@ public class NewCustBeanClass {
         return it27;
     }
 
-    public void setIt28(RichInputText it28) {
-        this.it28 = it28;
-    }
-
-    public RichInputText getIt28() {
-        return it28;
-    }
-
-    public void setId3(RichInputDate id3) {
-        this.id3 = id3;
-    }
-
-    public RichInputDate getId3() {
-        return id3;
-    }
-
-    public void setIt29(RichInputText it29) {
-        this.it29 = it29;
-    }
-
-    public RichInputText getIt29() {
-        return it29;
-    }
-
-    public void setId4(RichInputDate id4) {
-        this.id4 = id4;
-    }
-
-    public RichInputDate getId4() {
-        return id4;
-    }
-
-    public void setIt30(RichInputText it30) {
-        this.it30 = it30;
-    }
-
-    public RichInputText getIt30() {
-        return it30;
-    }
-
-    public void setIt31(RichInputText it31) {
-        this.it31 = it31;
-    }
-
-    public RichInputText getIt31() {
-        return it31;
-    }
-
-    public void setIt32(RichInputText it32) {
-        this.it32 = it32;
-    }
-
-    public RichInputText getIt32() {
-        return it32;
-    }
 
     public void setIt33(RichInputText it33) {
         this.it33 = it33;
@@ -467,22 +298,7 @@ public class NewCustBeanClass {
         return it39;
     }
 
-    public void setIt40(RichInputText it40) {
-        this.it40 = it40;
-    }
 
-    public RichInputText getIt40() {
-        return it40;
-    }
-
-    public void setIt41(RichInputText it41) {
-        this.it41 = it41;
-    }
-
-    public RichInputText getIt41() {
-        return it41;
-    }
-    
     public String commit_now() {
             System.out.println("Executing commit_now()");
             BindingContainer bindings = BindingContext.getCurrent().getCurrentBindingsEntry();
@@ -493,8 +309,16 @@ public class NewCustBeanClass {
             DCIteratorBinding iter = (DCIteratorBinding)bindings2.findIteratorBinding("MasterTable1Iterator");
             RowSetIterator rsi = iter.getRowSetIterator();
             Row row = rsi.getCurrentRow();
+            
+            try{
+                if(row.getAttribute("AddressId1")=="y"){
+                    row.setAttribute("PrimaryAddressId",row.getAttribute("AddressId1"));
+                }
+            }catch(Exception e){
+                e.getStackTrace();
+            }
 
-            row.setAttribute("PrimaryAddressId",row.getAttribute("AddressId1"));
+            
             row.setAttribute("ShippingAddressId",row.getAttribute("AddressId1"));
             row.setAttribute("PrimaryContactId",row.getAttribute("ContactId"));
             row.setAttribute("CustomerId1",row.getAttribute("CustomerId"));
