23bc33b5686648fb7b090efd631a64f9c1ebd961
Arjun Madhu Rejina <arjun.madhu.rejina@oracle.com>
2017-08-22 21:49:32 +0530

    Delete popup for admin user

--- ViewController/src/view/SafeDeleteBean.java
+++ ViewController/src/view/SafeDeleteBean.java
@@ -2,6 +2,8 @@ package view;
 
 import oracle.adf.model.BindingContext;
 
+import oracle.adf.view.rich.event.DialogEvent;
+
 import oracle.binding.BindingContainer;
 import oracle.binding.OperationBinding;
 
@@ -13,6 +15,11 @@ public class SafeDeleteBean {
         return BindingContext.getCurrent().getCurrentBindingsEntry();
     }
 
+    public void dialogDeleteListenerUser(DialogEvent dialogEvent){
+        if (dialogEvent.getOutcome()==DialogEvent.Outcome.ok){
+            commit_Delete();
+        }
+    }
     public String commit_Delete() {
         BindingContainer bindings = getBindings();
         OperationBinding operationBinding = bindings.getOperationBinding("Delete");
@@ -25,10 +32,15 @@ public class SafeDeleteBean {
         if (!operationBinding2.getErrors().isEmpty()) {
             return null;
         }
-        System.out.println("Runnin delete");
+        
         return null;
     }
     
+    public void dialogDeleteListenerProduct(DialogEvent dialogEvent){
+        if (dialogEvent.getOutcome()==DialogEvent.Outcome.ok){
+            commit_Delete_Product();
+        }
+    }
     public String commit_Delete_Product() {
         BindingContainer bindings = getBindings();
         OperationBinding operationBinding = bindings.getOperationBinding("Delete1");
@@ -45,6 +57,12 @@ public class SafeDeleteBean {
         return null;
     }
 
+    public void dialogDeleteListenerRole(DialogEvent dialogEvent){
+        if (dialogEvent.getOutcome()==DialogEvent.Outcome.ok){
+            deleteRole();
+        }
+    }
+    
     public String deleteRole() {
         BindingContainer bindings = getBindings();
         OperationBinding operationBinding = bindings.getOperationBinding("Delete2");
