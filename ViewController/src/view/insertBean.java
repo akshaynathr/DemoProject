16819ede42f0681b3503b5d43462b9fc4569e92e
akramana <akramana@AKRAMANA-LAP.oradev.oraclecorp.com>
2017-08-25 03:06:55 +0530

    added insertion for revenue items

--- ViewController/src/view/insertBean.java
+++ ViewController/src/view/insertBean.java
@@ -60,4 +60,39 @@ public class insertBean {
                     
                 }
     }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  
+    public String insertOPRevenueItemCommitAction() {
+       System.out.print("Test");
+       BindingContainer bindings = getBindings();
+    //                   OperationBinding operationBinding = bindings.getOperationBinding("CreateInsert3");
+    //                   Object result = operationBinding.execute();
+    //                   if (!operationBinding.getErrors().isEmpty()) {
+    //                       return null;
+    //                   }
+       
+       OperationBinding operationBinding2 = bindings.getOperationBinding("Commit");
+       Object result2 = operationBinding2.execute();
+       if (!operationBinding2.getErrors().isEmpty()) {
+           return null;
+       }
+       
+       return null;
+    }
+
+
+    public void insertOpRevenueItemPopFetch(PopupFetchEvent popupFetchEvent) {
+    // Add event code here...
+    
+    BindingContainer bindings = getBindings();
+            OperationBinding operationBinding = bindings.getOperationBinding("CreateInsert4");
+            Object result = operationBinding.execute();
+            if (!operationBinding.getErrors().isEmpty()) {
+                
+            }
+    }
+    
+    
+
 }
